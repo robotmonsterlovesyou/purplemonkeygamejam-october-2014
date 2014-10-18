ed0353326658552b646f13b019d5fdf0d0f5fa39
Scott Doxey <neogeek0101@gmail.com>
2014-10-18 12:28:07 -0400

    Initial commit.

---
+++ bower_components/facade.js/CHANGELOG.md
@@ -0,0 +1,42 @@
+#Changelog
+
+##0.3.0-beta.6 (October 2, 2014)
+
+- Fixed issue when clearing a canvas with no width/height attributes.
+- Updated documentation to work with latest version of doxdox.
+
+##0.3.0-beta.5 (July 24, 2014)
+
+- Fix for offsetX/offsetY regression bug.
+- Improved image offset support with regards to sprite wrapping.
+
+##0.3.0-beta.4 (July 2, 2014)
+
+- Fixed bug introduced in 0.3.0-beta.3 with text wrapping in Facade.Text.
+
+##0.3.0-beta.3 (July 2, 2014)
+
+- Performance updates.
+- Documentation updates.
+
+###Bug Fixes
+
+- Fixed issue found in Firefox where drawImage would scale to fill if offsetX or offsetY cropped the image to only render a subset of the image's width and/or height.
+- Fixed issue where fillStyle would be applied to Facade.Text even when empty.
+- Fixed issue where Facade.Text.setText wouldn't store the new value in the `value` property.
+
+###New Features
+
+- Facade.Image: Added sprite wrapping.
+
+###Deprecated
+
+- Facade.Entity: `shadowBlur`, `shadowColor`, `shadowOffsetX`, `shadowOffsetY`.
+
+##0.3.0-beta.2 (June 15, 2014)
+
+- Fixed issues with importing through [requirejs](http://requirejs.org/) and [node](http://nodejs.org/).
+
+##0.3.0-beta (June 9, 2014)
+
+- Initial public release.
---
+++ js/app.js
@@ -0,0 +1,63 @@
+/*jslint browser: true */
+/*globals Facade, Gamepad, $ */
+
+define(function (require) {
+
+    'use strict';
+
+    var $ = require('jquery'),
+        Facade = require('facade'),
+        Gamepad = require('gamepadjs'),
+        stage = new Facade(document.querySelector('canvas')),
+        controls = new Gamepad(),
+        world,
+        player;
+
+    require('facadejs-Box2D-plugin');
+
+    world = new Facade.Entity().Box2D('createWorld', { canvas: stage.canvas, gravity: [ 0, 0 ] });
+
+    player = new Facade.Rect({ x: stage.width() / 2, y: stage.height() / 2, width: 10, height: 10, fillStyle: 'red' });
+
+    player.Box2D('createObject', world, {
+        type: 'dynamic',
+        friction: 3
+    });
+
+    stage.draw(function () {
+
+        this.clear();
+
+        this.addToStage(player);
+
+        world.Box2D('step');
+
+        world.Box2D('drawDebug');
+
+    });
+
+    controls.on('hold', 'stick_axis_left', function (e) {
+
+        if (e.value[0] < 0) {
+
+            player.Box2D('setVelocity', -5, null);
+
+        } else if (e.value[0]) {
+
+            player.Box2D('setVelocity', 5, null);
+
+        }
+
+        if (e.value[1] < 0) {
+
+            player.Box2D('setVelocity', null, -5);
+
+        } else if (e.value[1]) {
+
+            player.Box2D('setVelocity', null, 5);
+
+        }
+
+    });
+
+});
