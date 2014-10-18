265ab4c41d337d3342b15ee8600631f3edd546ca
Scott Doxey <neogeek0101@gmail.com>
2014-10-18 12:29:44 -0400

    Fixed path issues.

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 require.config({
     'paths': {
-        'box2dweb': '/bower_components/facadejs-Box2D-plugin/vendor/box2dweb/Box2dWeb-2.1.a.3',
-        'facade': '/bower_components/facade.js/facade.min',
-        'facadejs-Box2D-plugin': '/bower_components/facadejs-Box2D-plugin/facadejs-Box2D',
-        'gamepadjs': '/bower_components/gamepad.js/gamepad.min',
+        'box2dweb': '../bower_components/facadejs-Box2D-plugin/vendor/box2dweb/Box2dWeb-2.1.a.3',
+        'facade': '../bower_components/facade.js/facade.min',
+        'facadejs-Box2D-plugin': '../bower_components/facadejs-Box2D-plugin/facadejs-Box2D',
+        'gamepadjs': '../bower_components/gamepad.js/gamepad.min',
         'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min'
     },
     'shim': {
