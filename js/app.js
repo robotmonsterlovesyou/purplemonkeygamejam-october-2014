768af7e84c289963acdf3e2655b94f7ece9f01d1
Scott Doxey <neogeek0101@gmail.com>
2014-10-18 13:25:17 -0400

    Moved player controls to entities folder.

--- js/app.js
+++ js/app.js
@@ -5,72 +5,16 @@ define(function (require) {
 
     'use strict';
 
-    var $ = require('jquery'),
-        Facade = require('facade'),
-        Gamepad = require('gamepadjs'),
+    var Facade = require('facade'),
         stage = new Facade(document.querySelector('canvas')),
-        controls = new Gamepad(),
-        player;
-
-    require('facadejs-SATjs-plugin');
-
-    player = new Facade.Rect({ x: stage.width() / 2, y: stage.height() / 2, width: 10, height: 10, fillStyle: 'red' });
-
-    player.SAT('setVector');
+        playerEntity = require('entities/player'),
+        player1 = new playerEntity({ x: stage.width() / 2, y: stage.height() / 2});
 
     stage.draw(function () {
 
         this.clear();
 
-        this.addToStage(player);
-
-    });
-
-    controls.on('hold', 'd_pad_left', function () {
-
-        player.setOptions({ x: '-=5' });
-
-    });
-
-    controls.on('hold', 'd_pad_right', function () {
-
-        player.setOptions({ x: '+=5' });
-
-    });
-
-    controls.on('hold', 'd_pad_up', function () {
-
-        player.setOptions({ y: '-=5' });
-
-    });
-
-    controls.on('hold', 'd_pad_down', function () {
-
-        player.setOptions({ y: '+=5' });
-
-    });
-
-    controls.on('hold', 'stick_axis_left', function (e) {
-
-        if (e.value[0] < -0.5) {
-
-            controls.trigger('hold', 'd_pad_left');
-
-        } else if (e.value[0] > 0.5) {
-
-            controls.trigger('hold', 'd_pad_right');
-
-        }
-
-        if (e.value[1] < -0.5) {
-
-            controls.trigger('hold', 'd_pad_up');
-
-        } else if (e.value[1] > 0.5) {
-
-            controls.trigger('hold', 'd_pad_down');
-
-        }
+        this.addToStage(player1);
 
     });
 
---
+++ js/entities/player.js
@@ -0,0 +1,84 @@
+/*jslint browser: true */
+/*globals Facade, Gamepad, $ */
+
+define(function (require) {
+
+    'use strict';
+
+    var Facade = require('facade'),
+        Gamepad = require('gamepadjs'),
+        controls = new Gamepad();
+
+    return function (pos) {
+
+        if (!pos) {
+
+            pos = { x: 0, y: 0 };
+
+        }
+
+        var player = new Facade.Rect({
+            x: pos.x,
+            y: pos.y,
+            width: 10,
+            height: 10,
+            fillStyle: 'red'
+        });
+
+        require('facadejs-SATjs-plugin');
+
+        player.SAT('setVector');
+
+        controls.on('hold', 'd_pad_left', function () {
+
+            player.setOptions({ x: '-=5' });
+
+        });
+
+        controls.on('hold', 'd_pad_right', function () {
+
+            player.setOptions({ x: '+=5' });
+
+        });
+
+        controls.on('hold', 'd_pad_up', function () {
+
+            player.setOptions({ y: '-=5' });
+
+        });
+
+        controls.on('hold', 'd_pad_down', function () {
+
+            player.setOptions({ y: '+=5' });
+
+        });
+
+        controls.on('hold', 'stick_axis_left', function (e) {
+
+            if (e.value[0] < -0.5) {
+
+                controls.trigger('hold', 'd_pad_left');
+
+            } else if (e.value[0] > 0.5) {
+
+                controls.trigger('hold', 'd_pad_right');
+
+            }
+
+            if (e.value[1] < -0.5) {
+
+                controls.trigger('hold', 'd_pad_up');
+
+            } else if (e.value[1] > 0.5) {
+
+                controls.trigger('hold', 'd_pad_down');
+
+            }
+
+        });
+
+        return player;
+
+    }
+
+});
