ecca7a4b8199ef02217a59e1ccbc7bbc4ac59f5e
Scott Doxey <neogeek0101@gmail.com>
2014-10-19 02:08:38 -0400

    Updated the bullet entity to use the entity module.

--- js/entities/bullet.js
+++ js/entities/bullet.js
@@ -1,36 +1,46 @@
 /*jslint browser: true */
-/*globals Facade, Gamepad, $ */
+/*globals Facade */
 
 define(function (require) {
 
     'use strict';
 
     var Facade = require('facade'),
-        Gamepad = require('gamepadjs'),
         Utils = require('utils'),
         sfx = require('sfx'),
         world = require('entities/world'),
         camera = require('entities/camera'),
-        controls = new Gamepad();
+        entityEntity = require('entities/entity');
+
+    require('facadejs-SATjs-plugin');
 
     return function (faction, pos, direction, shipVel) {
 
-        if (!pos) {
-
-            pos = { x: 0, y: 0 };
-
-        }
-
-        var bullet,
+        var bullet = new entityEntity('bullets', faction),
             shipVelC = Utils.polarToCart(shipVel.mag, shipVel.dir),
             bulletVelC = Utils.polarToCart(7, direction),
             bulletVelP = Utils.cartToPolar(shipVelC.x + bulletVelC.x, shipVelC.y + bulletVelC.y),
             bulletSFX = new sfx('sfx/laser.ogg').volume(.5);
 
-        function detectHit () {
+        bullet.setSprite(new Facade.Polygon({
+            points: [[0,3],[6,0],[0,-3],[-6,0]],
+            x: pos.x,
+            y: pos.y,
+            lineJoin: 'round',
+            anchor: 'top',
+            rotate: direction * 180 / Math.PI,
+            fillStyle: '#faa'
+        }));
+
+        bullet.velocity = {
+            mag: bulletVelP.radius,
+            dir: bulletVelP.angle,
+        };
+
+        bullet.detectHit = function () {
 
             var hit = false,
-                bulletVector = bullet.sprite.SAT('getVector');
+                bulletVector = this.sprite.SAT('getVector');
 
             world.entities.factions.enemies.forEach(function (npc) {
 
@@ -48,57 +58,33 @@ define(function (require) {
 
             return hit;
 
-        }
-
-        bullet = {
-            sprite: new Facade.Polygon({
-                points: [[0,3],[6,0],[0,-3],[-6,0]],
-                x: pos.x,
-                y: pos.y,
-                lineJoin: 'round',
-                anchor: 'top',
-                rotate: direction * 180 / Math.PI,
-                fillStyle: '#faa'
-            }),
-            velocity: {
-                mag: bulletVelP.radius,
-                dir: bulletVelP.angle,
-            },
-            destroy: function () {
-
-                world.entities.bullets[faction].splice(world.entities.bullets[faction].indexOf(bullet), 1);
-
-            },
-            update: function () {
-
-                var move = Utils.polarToCart(this.velocity.mag, this.velocity.dir),
-                    newPos = {
-                        x: this.sprite.getOption('x') + move.x,
-                        y: this.sprite.getOption('y') + move.y
-                    };
-
-                this.sprite.setOptions({
-                    x: newPos.x,
-                    y: newPos.y,
-                    rotate: this.velocity.dir * 180 / Math.PI
-                });
-
-                if (!camera.isVisible(bullet) || detectHit()) {
-
-                    bullet.destroy();
-
-                } else {
-
-                    bullet.sprite.SAT('setVector');
-
-                }
-
-            }
         };
 
-        require('facadejs-SATjs-plugin');
+        bullet.update = function () {
 
-        bullet.sprite.SAT('setVector');
+            var move = Utils.polarToCart(this.velocity.mag, this.velocity.dir),
+                newPos = {
+                    x: this.sprite.getOption('x') + move.x,
+                    y: this.sprite.getOption('y') + move.y
+                };
+
+            this.sprite.setOptions({
+                x: newPos.x,
+                y: newPos.y,
+                rotate: this.velocity.dir * 180 / Math.PI
+            });
+
+            if (!camera.isVisible(bullet) || this.detectHit()) {
+
+                bullet.destroy();
+
+            } else {
+
+                bullet.sprite.SAT('setVector');
+
+            }
+
+        }
 
         bulletSFX.play();
 
