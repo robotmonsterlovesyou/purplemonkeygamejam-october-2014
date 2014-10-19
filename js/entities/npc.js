609a162efee4b196880adc39616e5aff409859b0
Scott Doxey <neogeek0101@gmail.com>
2014-10-19 10:48:21 -0400

    Updated npc to use new ship entity.

---
+++ js/entities/npc.js
@@ -0,0 +1,18 @@
+/*jslint browser: true */
+/*globals Facade */
+
+define(function (require) {
+
+    'use strict';
+
+    var shipEntity = require('entities/ship');
+
+    return function (faction, pos, direction) {
+
+        var npc = new shipEntity(faction, pos, direction);
+
+        return npc;
+
+    }
+
+});
