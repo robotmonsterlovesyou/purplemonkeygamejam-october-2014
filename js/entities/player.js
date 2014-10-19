45f02411db59ad4167d3d32de1d3a3bfbfa96ad7
Scott Doxey <neogeek0101@gmail.com>
2014-10-19 15:06:56 -0400

    Added player death and respond.

--- js/entities/player.js
+++ js/entities/player.js
@@ -11,12 +11,88 @@ define(function (require) {
         Gamepad = require('gamepadjs'),
         world = require('entities/world'),
         camera = require('entities/camera'),
+        ui = require('entities/ui'),
         shipEntity = require('entities/ship'),
         controls = new Gamepad();
 
     require('facadejs-SATjs-plugin');
 
+    controls.setCustomMapping('keyboard', {
+        'key_a': 65,
+        'key_w': 87,
+        'key_s': 83,
+        'key_d': 68,
+        'd_pad_left': 37,
+        'd_pad_right': 39,
+        'd_pad_up': 38,
+        'd_pad_down': 40
+    });
+
+    controls.on('hold', 'key_a', function (e) {
+
+        console.log(world.activeState.players);
+
+        world.activeState.players[0].updateMove(-1, 0);
+
+    });
+
+    controls.on('hold', 'key_d', function (e) {
+
+        world.activeState.players[0].updateMove(1, 0);
+
+    });
+
+    controls.on('hold', 'key_w', function (e) {
+
+        world.activeState.players[0].updateMove(0, -1);
+
+    });
+
+    controls.on('hold', 'key_s', function (e) {
+
+        world.activeState.players[0].updateMove(0, 1);
+
+    });
+
+    controls.on('hold', 'd_pad_left', function (e) {
+
+        world.activeState.players[0].updateWeapon(-1, 0);
+
+    });
+
+    controls.on('hold', 'd_pad_right', function (e) {
+
+        world.activeState.players[0].updateWeapon(1, 0);
+
+    });
+
+    controls.on('hold', 'd_pad_up', function (e) {
+
+        world.activeState.players[0].updateWeapon(0, -1);
+
+    });
+
+    controls.on('hold', 'd_pad_down', function (e) {
+
+        world.activeState.players[0].updateWeapon(0, 1);
+
+    });
+
+    controls.on('hold', 'stick_axis_left', function (e) {
+
+        world.activeState.players[0].updateMove(e.value[0], e.value[1], stickThreshold);
+
+    });
+
+    controls.on('hold', 'stick_axis_right', function (e) {
+
+        world.activeState.players[0].updateWeapon(e.value[0], e.value[1], stickThreshold);
+
+    });
+
     return function (type, pos, direction, shipVel) {
+
+        console.log('new player');
 
         var player = new shipEntity('npcs', type, pos, direction, shipVel),
             stickThreshold = 0.3;
@@ -38,7 +114,30 @@ define(function (require) {
         player.weapon.dir = Math.PI * 3 / 2;
 
         // inheritance workaround
+        player.destroyShip = player.destroy;
         player.updateShip = player.update;
+
+        player.destroy = function () {
+
+            ui.stats.lives--;
+
+            if (ui.stats.lives) {
+
+                setTimeout(function () {
+
+                    world.activeState.spawnPlayer(0);
+
+                    controls.resume();
+
+                }, 1000);
+
+            }
+
+            controls.pause();
+
+            player.destroyShip();
+
+        };
 
         player.update = function () {
 
@@ -46,78 +145,6 @@ define(function (require) {
             camera.followEntity(this);
 
         };
-
-        controls.setCustomMapping('keyboard', {
-
-            'key_a': 65,
-            'key_w': 87,
-            'key_s': 83,
-            'key_d': 68,
-            'd_pad_left': 37,
-            'd_pad_right': 39,
-            'd_pad_up': 38,
-            'd_pad_down': 40
-        });
-
-        controls.on('hold', 'key_a', function (e) {
-
-            player.updateMove(-1, 0);
-
-        });
-
-        controls.on('hold', 'key_d', function (e) {
-
-            player.updateMove(1, 0);
-
-        });
-
-        controls.on('hold', 'key_w', function (e) {
-
-            player.updateMove(0, -1);
-
-        });
-
-        controls.on('hold', 'key_s', function (e) {
-
-            player.updateMove(0, 1);
-
-        });
-
-        controls.on('hold', 'd_pad_left', function (e) {
-
-            player.updateWeapon(-1, 0);
-
-        });
-
-        controls.on('hold', 'd_pad_right', function (e) {
-
-            player.updateWeapon(1, 0);
-
-        });
-
-        controls.on('hold', 'd_pad_up', function (e) {
-
-            player.updateWeapon(0, -1);
-
-        });
-
-        controls.on('hold', 'd_pad_down', function (e) {
-
-            player.updateWeapon(0, 1);
-
-        });
-
-        controls.on('hold', 'stick_axis_left', function (e) {
-
-            player.updateMove(e.value[0], e.value[1], stickThreshold);
-
-        });
-
-        controls.on('hold', 'stick_axis_right', function (e) {
-
-            player.updateWeapon(e.value[0], e.value[1], stickThreshold);
-
-        });
 
         return player;
 
